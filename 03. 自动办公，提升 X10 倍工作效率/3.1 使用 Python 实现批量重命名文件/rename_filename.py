0c5c5e49a0b5a623513a9039330442fc9fe03e68
aiyc <1432803776@qq.com>
2021-09-24 13:48:55 +0800

    update

---
+++ 03. 自动办公，提升 X10 倍工作效率/3.1 使用 Python 实现批量重命名文件/rename_filename.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Author: AI悦创
+# @Date:   2021-09-24 10:52:01
+# @Last Modified by:   aiyc
+# @Last Modified time: 2021-09-24 12:50:02
+import os
+
+root, dirs, files = next(os.walk("tips_3/"))
+
+idx = 0
+
+for item in files:
+	old_file_path = os.path.join(root,item)
+	new_file_path = os.path.join(root, f"aiyc_{idx}.jpg")
+	os.rename(old_file_path, new_file_path)
+	idx = idx + 1
